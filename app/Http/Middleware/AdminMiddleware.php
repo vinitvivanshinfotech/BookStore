e8f185aee09c20100281084416fe912c0cb6f40e
Vinit VI <vinit.m@vivanshinfotech.com>
2024-02-29 15:09:42 +0530

    changeinroute

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -19,7 +19,7 @@ class AdminMiddleware
     public function handle(Request $request, Closure $next): Response
     {
         if(!Auth()->guard('admin')->check()){
-            return redirect()->route('admin.login');
+            return redirect()->route('admin.loginForm');
         }
         return $next($request);
     }
