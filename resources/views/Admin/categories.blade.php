ecb3ae1e91240217bc553b86c77cb1d549bb3555
Vinit VI <vinit.m@vivanshinfotech.com>
2024-03-08 14:42:12 +0530

    changes in desgin

--- resources/views/Admin/categories.blade.php
+++ resources/views/Admin/categories.blade.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{__('adminlabel.showallbook')}}</title>
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
+
 
     @include('cdn')
 </head>
@@ -12,6 +14,8 @@
 <body>
     @include('Admin.layoutAdmin.navbar')
     @include('Admin.layoutAdmin.sildebar')
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
     <div class="form-floating mb-3">
         <select class="form-select optionselect" name="Order Status">
             <option value="None">None</option>
@@ -45,18 +49,76 @@
     <script>
         $(document).ready(function() {
 
+            var $table = $('#book_list'),
+                dataTable;
+            var data = {};
+
+            function dataTableReload() {
+                dataTable.ajax.reload();
+            };
+
+            function dataTableInit(data) {
+
+                dataTable = $table.DataTable({
+                    "responsive": true,
+                    "processing": true,
+                    "serverSide": true,
+                    "destroy": true,
+                    "stateSave": true,
+                    
+                    "colums": [{
+                            data: 'id'
+                        },
+                        {
+                            data: 'author_name'
+                        },
+                        {
+                            data: 'book_title'
+                        },
+                        {
+                            data: 'author_name'
+                        },
+                        {
+                            data: 'author_email'
+                        },
+                        {
+                            data: 'book_edition'
+                        },
+                        {
+                            data: 'book_cover'
+                        },
+                        {
+                            data: 'book_language'
+                        },
+                    ],
+                    "order": [
+                        [0, 'desc']
+                    ],
+                    "pageLength": 10,
+                    "lengthMenu": [
+                        [10, 25, 50, 100, -1],
+                        [10, 25, 50, 100, "All"]
+                    ],
+
+                });
+            }
+            dataTableReload();
+
             $('.optionselect').change(function() {
                 var categories = $(this).val();
                 // console.log(categories)
                 $.ajax({
-                    type: "POST",
+                    type: "GET",
                     url: "{{route('category.store')}}",
                     data: {
                         categories: categories,
                         _token: '{{csrf_token()}}'
                     },
-                    success: function(response) {
-                        console.log(response);
+                    dataType: 'json',
+                    success: function(book_types) {
+                        console.log(book_types);
+                        dataTableInit(book_types);
+
                     },
                     error: function(response) {
                         console.log('error');
@@ -64,6 +126,9 @@
                     }
                 });
             });
+
+
+
         });
     </script>
 </body>
